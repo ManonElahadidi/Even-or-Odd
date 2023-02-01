b8e81374983d91b050a7983a1638593752629ce8
ManonElahadidi <124053986+ManonElahadidi@users.noreply.github.com>
2023-02-01 02:07:40 +0200

    Add files via upload

---
+++ even or odd/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int num ;
+     printf("Enter the number :");
+   scanf("%d", &num );
+   if (num % 2== 0)
+   {
+       printf( "Even Number" );
+
+
+}
+   else if (num % 2== 1)
+   {
+    printf( "Odd Number" );
+   }
+
+}
